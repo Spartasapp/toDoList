4e3bf541ddca9dd4fbb7a53ea846d3ce5d694a74
Siarhei <spartateambuilder@mail.ru>
2019-09-11 11:50:53 +0300

    Updates

---
+++ precache-manifest.11d2da41c4c2f46958db60a82ab5a6c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e94140bf7b28114665b2519209756e",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "bb1dbdfd8bd68d026b99",
+    "url": "/toDoList/static/css/main.4eca6905.chunk.css"
+  },
+  {
+    "revision": "1cdc2d637942cb89b265",
+    "url": "/toDoList/static/js/2.4be1710b.chunk.js"
+  },
+  {
+    "revision": "bb1dbdfd8bd68d026b99",
+    "url": "/toDoList/static/js/main.e1b97b1e.chunk.js"
+  },
+  {
+    "revision": "9722a55e69eb4ad6e976",
+    "url": "/toDoList/static/js/runtime~main.930cf86c.js"
+  },
+  {
+    "revision": "aa3813e402360f10ddd4581c0cacad65",
+    "url": "/toDoList/static/media/preloader.aa3813e4.svg"
+  }
+]);
